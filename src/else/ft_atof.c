246c9f3c425fdfe52e846643202a7ad5e41471a4
Apouchet <AntoinePouchet@MacBook-Pro-de-Antoine.local>
2019-02-19 16:31:01 +0100

    correction ft_atof

--- src/else/ft_atof.c
+++ src/else/ft_atof.c
@@ -6,7 +6,7 @@
 /*   By: apouchet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 16:09:26 by apouchet          #+#    #+#             */
-/*   Updated: 2019/02/19 16:21:29 by apouchet         ###   ########.fr       */
+/*   Updated: 2019/02/19 16:30:14 by apouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ float	ft_atof(char *s)
 	while (*s == '0')
 		s++;
 	a = ft_atoi(s);
-	if (*s && *s != '.')
+	while (*s && *s != '.')
 		s++;
 	if (*s && ft_isdigit(s[1]))
 		s++;
@@ -34,5 +34,5 @@ float	ft_atof(char *s)
 		return (0);
 	while (ft_isdigit(s[i]))
 		i++;
-	return (a + (ft_atoi(s) / ft_pow(10, i)));
+	return (a + (ft_atoi(s) / (float)ft_pow(10, i)));
 }
